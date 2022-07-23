da6818d84f968f7fab73d8cda6bc96a2e50788ff
Anton Balter <antbalter@gmail.com>
2022-07-23 23:05:57 +0300

    AD05-solution

---
+++ src/ad05/Lottery.java
@@ -0,0 +1,38 @@
+package ad05;
+
+public class Lottery {
+	private int[] winningCombination;
+
+	public Lottery(int[] winningCombination) {
+		this.winningCombination = winningCombination;
+	}
+
+	public int rateCombination(int playerCombination[]) {
+		int[] helper = new int[50];
+		int rate = 0;
+		for(int a: winningCombination) {
+			helper[a] = 2;			
+		}
+		for(int a: playerCombination) {
+			--helper[a];
+			if (helper[a] == 1) {
+				rate++;
+			}
+		}
+		return rate;
+	}
+
+	public static void main(String[] args) {
+		Lottery loto = new Lottery(new int[]{5,2,17,48,43,7,9}) ;
+		if (loto.rateCombination(new int[] {3,5,12,17,44,10,7}) != 3) {
+			System.out.println("Test 1 failed");
+           return;			
+		}
+		if (loto.rateCombination(new int[] {3,5,12,17,49,9,7}) != 5) {
+			System.out.println("Test 2 failed");
+           return;			
+		}
+		System.out.println("Success");
+	}
+}
+
